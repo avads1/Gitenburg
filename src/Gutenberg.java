70df8ec348309f99a261adecdbbc67e0dcdf83b3
Greeshma <ava.greeshma13@gmail.com>
2020-07-20 20:48:41 -0400

    Clean code

--- src/Gutenberg.java
+++ src/Gutenberg.java
@@ -1,57 +1,90 @@
-import java.io.IOException;
+import java.io.*;
+import java.util.Map;
 
 public class Gutenberg {
     public static void main(String[] args) throws IOException {
-        String filePath = "C:\\Users\\grees\\Documents\\Codepath\\Gutenberg\\src\\JaneAusten.txt";
-        String commonWordsPath = "C:\\Users\\grees\\Documents\\Codepath\\Gutenberg\\src\\1000Freq.txt";
-        Utils util = new Utils(filePath, commonWordsPath);
-//        int totalWords = util.getTotalNumberOfWords();
-//        System.out.println("Total num of words : " + totalWords);
-//        System.out.println("=========================");
-//
-//        int uniqueWords = util.getTotalUniqueWords();
-//        System.out.println("Total num of unique words : " + uniqueWords);
-//        System.out.println("=========================");
-//
-//        Map<String, Integer> top20Freq = util.get20MostFrequentWords();
-//        System.out.println("Top 20 freq words : ");
-//        int i=1;
-//        for (Map.Entry<String, Integer> entry : top20Freq.entrySet()) {
-//            System.out.println((i++)+". "+entry.getKey() + " " + entry.getValue());
-//        }
-//        System.out.println("=========================");
-//        Map<String, Integer> top20LeastFrequent = util.top20LeastFrequent();;
-//        i=1;
-//        System.out.println("Top 20 Least Frequent words : ");
-//        for (Map.Entry<String, Integer> entry : top20LeastFrequent.entrySet()) {
-//            System.out.println((i++)+". "+entry.getKey() + " " + entry.getValue());
-//        }
-//        System.out.println("=========================");
-//
-//        Map<String, Integer> top20Interesting = util.get20MostInterestingWords();;
-//        i=1;
-//        System.out.println("Top 20 Interesting words : ");
-//        for (Map.Entry<String, Integer> entry : top20Interesting.entrySet()) {
-//            System.out.println((i++)+". "+entry.getKey() + " " + entry.getValue());
-//        }
-//        System.out.println("=========================");
-//        String searchWord = "Bennet";
-//        int[] frequencyInEachChapter = util.getFrequencyOfWord(searchWord);
-//        for(int p=0;p<frequencyInEachChapter.length;p++){
-//            System.out.print(" "+frequencyInEachChapter[p]+" ");
-//        }
+        String currentDirectory = System.getProperty("user.dir");
+        System.out.println("The current working directory is " + currentDirectory);
+        String filePath = currentDirectory+"\\src\\JaneAusten.txt";
+        String commonWordsPath = currentDirectory+"\\src\\1000Freq.txt";
+        FileWriter fileWriter = new FileWriter(currentDirectory+"\\src\\output.txt");
+        File file = new File(filePath);
+        File commonWordFile = new File(commonWordsPath);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+        StringBuilder sb = new StringBuilder();
+        while ((st = br.readLine()) != null) {
+            if (st.isEmpty()) {
+                continue;
+            }
+            sb.append(st.trim() + " ");
+        }
+        String fullText = sb.toString();
+        fullText = fullText.replaceAll("[\uFEFF-\uFFFF]", "");
+        br = new BufferedReader(new FileReader(commonWordFile));
+        sb = new StringBuilder();
+        while ((st = br.readLine()) != null) {
+            if (st.isEmpty()) {
+                continue;
+            }
+            sb.append(st.trim() + " ");
+        }
+        String commonText = sb.toString();
+        Utils util = new Utils(fullText, commonText);
 
-        System.out.println("=========================");
+        int totalWords = util.getTotalNumberOfWords(fullText);
+        fileWriter.write("Total num of words : " + totalWords+"\n");
+        fileWriter.write("=========================\n");
+
+        int uniqueWords = util.getTotalUniqueWords(fullText);
+        fileWriter.write("Total num of unique words : " + uniqueWords+"\n");
+        fileWriter.write("=========================\n");
+
+        Map<String, Integer> top20Freq = util.get20MostFrequentWords(fullText);
+        fileWriter.write("Top 20 freq words : \n");
+        int i = 1;
+        for (Map.Entry<String, Integer> entry : top20Freq.entrySet()) {
+            fileWriter.write((i++) + ". " + entry.getKey() + " " + entry.getValue());
+            fileWriter.write("\n");
+        }
+        fileWriter.write("=========================\n");
+        Map<String, Integer> top20LeastFrequent = util.top20LeastFrequent();
+        ;
+        i = 1;
+        fileWriter.write("Top 20 Least Frequent words : \n");
+        for (Map.Entry<String, Integer> entry : top20LeastFrequent.entrySet()) {
+            fileWriter.write((i++) + ". " + entry.getKey() + " " + entry.getValue());
+            fileWriter.write("\n");
+        }
+        fileWriter.write("=========================\n");
+
+        Map<String, Integer> top20Interesting = util.get20MostInterestingWords(fullText);
+        ;
+        i = 1;
+        fileWriter.write("Top 20 Interesting words : \n");
+        for (Map.Entry<String, Integer> entry : top20Interesting.entrySet()) {
+            fileWriter.write((i++) + ". " + entry.getKey() + " " + entry.getValue());
+            fileWriter.write("\n");
+        }
+        fileWriter.write("=========================\n");
+        String searchWord = "Bennet";
+        int[] frequencyInEachChapter = util.getFrequencyOfWord(searchWord);
+        fileWriter.write("Frequency in each chapter : \n");
+        for (int p = 0; p < frequencyInEachChapter.length; p++) {
+            fileWriter.write(" " + frequencyInEachChapter[p] + " ");
+        }
+        fileWriter.write("\n");
+        fileWriter.write("=========================\n");
         String quote = "Happiness in marriage is entirely a matter of chance.";
         int chapterNum = util.getChapterOfQuote(quote);
-        System.out.println("Quote : " + quote);
-        System.out.println("Chapter number : " + chapterNum);
+        fileWriter.write("Quote : " + quote+"\n");
+        fileWriter.write("Chapter number : " + chapterNum+"\n");
 
-        System.out.println("=========================");
+        fileWriter.write("=========================\n");
 
-        System.out.println("Generated sentence : ");
-        System.out.println(util.generateSentence());
-
+        fileWriter.write("Generated sentence : \n");
+        fileWriter.write(util.generateSentence());
+        fileWriter.close();
 
     }
 
--- src/Utils.java
+++ src/Utils.java
@@ -2,87 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Utils {
-    private String filePath;
-    private String mostCommonFile;
     private String fullText;
+    private String commonText;
 
-    Utils(String fPath, String freqFile) throws IOException {
-        filePath = fPath;
-        mostCommonFile = freqFile;
-        File file = new File(filePath);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
-        StringBuilder sb = new StringBuilder();
-        while ((st = br.readLine()) != null) {
-            if(st.isEmpty()){
-                continue;
-            }
-            sb.append(st.trim() + " ");
-        }
-        fullText = sb.toString();
+    Utils(String text, String common) throws IOException {
+        fullText = text;
+        commonText = common;
     }
 
-    public int getTotalNumberOfWords() throws IOException {
-        File file = new File(filePath);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
+    /*******************************PUBLIC METHODS***********************************************/
+
+    public int getTotalNumberOfWords(String text) throws IOException {
         int count = 0;
-        while ((st = br.readLine()) != null) {
-            st = st.trim();
-            String[] arr = st.split("\\s+");
-            if (arr.length == 1 && arr[0].isEmpty()) {
-                continue;
-            }
-            count = count + arr.length;
-        }
-        return count;
+        String[] arr = text.split("\\s+");
+        return arr.length;
     }
 
-    public int getTotalUniqueWords() throws IOException {
-        File file = new File(filePath);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
+    public int getTotalUniqueWords(String text) throws IOException {
         HashSet<String> set = new HashSet<>();
-        while ((st = br.readLine()) != null) {
-            st = st.trim();
-            String[] arr = st.split("\\s+");
-            if (arr.length == 1 && arr[0].isEmpty()) {
-                continue;
-            }
-            for (String s : arr) {
-                set.add(s);
-            }
+        String[] arr = text.split("\\s+");
+        for (String s : arr) {
+            set.add(s);
         }
-//        for(String s:set){
-//            System.out.println(s+" | ");
-//        }
         return set.size();
     }
 
-    public Map<String, Integer> get20MostFrequentWords() throws IOException {
+    public Map<String, Integer> get20MostFrequentWords(String text) throws IOException {
         HashMap<String, Integer> map = new HashMap<>();
-        File file = new File(filePath);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
-        while ((st = br.readLine()) != null) {
-            st = st.trim();
-            String[] arr = st.split("\\s+");
-            if (arr.length == 1 && arr[0].isEmpty()) {
-                continue;
-            }
-            for (String s : arr) {
-                String[] strArr = s.split("[\\p{Punct}\\s]+");
-//               // System.out.println(strArr.length+" | ");
-                if (strArr.length == 1)
-                    map.put(strArr[0], map.getOrDefault(strArr[0], 0) + 1);
-//                if (strArr.length == 1)
-//                    map.put(s, map.getOrDefault(s, 0) + 1);
-            }
+        text = text.trim();
+        String[] arr = text.split("\\s+");
+        for (String s : arr) {
+            String[] strArr = s.split("[\\p{Punct}\\s]+");
+            if (strArr.length == 1)
+                map.put(strArr[0], map.getOrDefault(strArr[0], 0) + 1);
         }
-
-//        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-//            System.out.println("map " + entry.getKey() + " " + entry.getValue());
-//        }
 
         TreeMap<Integer, List<String>> treeMap = new TreeMap<>(Collections.reverseOrder());
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
@@ -117,38 +70,21 @@ public class Utils {
         return map;
     }
 
-    public Map<String, Integer> get20MostInterestingWords() throws IOException {
+    public Map<String, Integer> get20MostInterestingWords(String text) throws IOException {
         HashMap<String, Integer> map = new HashMap<>();
         HashSet<String> topCommonWords = getMostCommonWords();
-        File file = new File(filePath);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
-        while ((st = br.readLine()) != null) {
-            st = st.trim();
-            String[] arr = st.split("\\s+");
-            if (arr.length == 1 && arr[0].isEmpty()) {
-                continue;
-            }
-            for (String s : arr) {
-                String[] strArr = s.split("[\\p{Punct}\\s]+");
-//               // System.out.println(strArr.length+" | ");
-                if (strArr.length == 1) {
-                    if (!strArr[0].equals("I")) {
-                        strArr[0] = strArr[0].toLowerCase();
-                    }
-                    if (!topCommonWords.contains(strArr[0]))
-                        map.put(strArr[0].toLowerCase(), map.getOrDefault(strArr[0].toLowerCase(), 0) + 1);
+        text = text.trim();
+        String[] arr = text.split("\\s+");
+        for (String s : arr) {
+            String[] strArr = s.split("[\\p{Punct}\\s]+");
+            if (strArr.length == 1) {
+                if (!strArr[0].equals("I")) {
+                    strArr[0] = strArr[0].toLowerCase();
                 }
-
-//                if (strArr.length == 1)
-//                    map.put(s, map.getOrDefault(s, 0) + 1);
+                if (!topCommonWords.contains(strArr[0]))
+                    map.put(strArr[0].toLowerCase(), map.getOrDefault(strArr[0].toLowerCase(), 0) + 1);
             }
         }
-        int i = 1;
-        /*for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            System.out.println((i++) + " map " + entry.getKey() + " " + entry.getValue());
-        }*/
-
         TreeMap<Integer, List<String>> treeMap = new TreeMap<>(Collections.reverseOrder());
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
             String str = entry.getKey();
@@ -180,50 +116,20 @@ public class Utils {
             }
         }
         return map;
-    }
-
-    private HashSet<String> getMostCommonWords() throws IOException {
-        File file = new File(mostCommonFile);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
-        HashSet<String> set = new HashSet<>();
-        while ((st = br.readLine()) != null) {
-            set.add(st);
-        }
-
-//        for(String s:set){
-//            System.out.println(s+"|");
-//        }
-        return set;
     }
 
     public Map<String, Integer> top20LeastFrequent() throws IOException {
         HashMap<String, Integer> map = new HashMap<>();
-//        File file = new File(filePath);
-//        BufferedReader br = new BufferedReader(new FileReader(file));
-//        String st;
-//        while ((st = br.readLine()) != null) {
-            String[] arr = fullText.split("\\s+");
-//            if (arr.length == 1 && arr[0].isEmpty()) {
-//                continue;
-//            }
-            for (String s : arr) {
-                String[] strArr = s.split("[\\p{Punct}\\s]+");
-//               // System.out.println(strArr.length+" | ");
-                if (strArr.length == 1){
-                    if(strArr[0].equals("Chapter")||strArr[0].matches("[0-9]+")){
-                        continue;
-                    }
-                    map.put(strArr[0], map.getOrDefault(strArr[0], 0) + 1);
+        String[] arr = fullText.split("\\s+");
+        for (String s : arr) {
+            String[] strArr = s.split("[\\p{Punct}\\s]+");
+            if (strArr.length == 1) {
+                if (strArr[0].equals("Chapter") || strArr[0].matches("[0-9]+")) {
+                    continue;
                 }
-
+                map.put(strArr[0], map.getOrDefault(strArr[0], 0) + 1);
             }
-//        }
-
-//        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-//            System.out.println("map " + entry.getKey() + " " + entry.getValue());
-//        }
-
+        }
         TreeMap<Integer, List<String>> treeMap = new TreeMap<>();
         for (Map.Entry<String, Integer> entry : map.entrySet()) {
             String str = entry.getKey();
@@ -262,57 +168,10 @@ public class Utils {
         int[] res = new int[numOfChapters];
         for (int i = 0; i < numOfChapters; i++) {
             String currChapterText = getTextOfChapter(i + 1);
-
             int occurrences = getWordFreqInText(currChapterText, word);
             res[i] = occurrences;
         }
         return res;
-    }
-
-    private int getWordFreqInText(String currChapterText, String word) {
-        String[] arr = currChapterText.split("[\\p{Punct}\\s]+");
-        int count = 0;
-        for (String s : arr) {
-            if (s.trim().equalsIgnoreCase(word)) {
-                count++;
-            }
-        }
-        return count;
-    }
-
-    private String getTextOfChapter(int chapterNumber) throws IOException {
-        File file = new File(filePath);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
-        StringBuilder sb = new StringBuilder();
-        boolean flag = false;
-        while ((st = br.readLine()) != null) {
-            if (st.contains("Chapter " + chapterNumber)) {
-                flag = true;
-            }
-            if (st.contains("Chapter " + (chapterNumber + 1))) {
-                flag = false;
-                return sb.toString();
-            }
-            if (flag) {
-                sb.append(st.trim() + " ");
-            }
-
-        }
-        return sb.toString();
-    }
-
-    private int getNumOfChapters() throws IOException {
-        File file = new File(filePath);
-        BufferedReader br = new BufferedReader(new FileReader(file));
-        String st;
-        int count = 0;
-        while ((st = br.readLine()) != null) {
-            if (st.contains("Chapter ")) {
-                count++;
-            }
-        }
-        return count;
     }
 
     public int getChapterOfQuote(String quote) throws IOException {
@@ -327,39 +186,100 @@ public class Utils {
         return -1;
     }
 
+
+    public String generateSentence() throws IOException {
+        String currWord = "the";
+        StringBuilder sb = new StringBuilder();
+        sb.append(currWord);
+        for (int i = 0; i < 19; i++) {
+            String[] arr = getNextWordList(currWord);
+            String nextWord = getRandomWord(arr);
+            //System.out.println(nextWord);
+            sb.append(" ").append(nextWord);
+            currWord = nextWord;
+        }
+        return sb.toString();
+    }
+
+    /**********************************HELPER FUNCTIONS***********************************************/
+
+    private HashSet<String> getMostCommonWords() throws IOException {
+        String[] arr = commonText.split("\\s+");
+        HashSet<String> set = new HashSet<>();
+        for (String st : arr) {
+            set.add(st);
+        }
+        return set;
+    }
+
+    private int getWordFreqInText(String currChapterText, String word) {
+        String[] arr = currChapterText.split("[\\p{Punct}\\s]+");
+        int count = 0;
+        for (String s : arr) {
+            if (s.equalsIgnoreCase(word)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private String getTextOfChapter(int chapterNumber) throws IOException {
+        int start = fullText.indexOf("Chapter " + (chapterNumber));
+        int end = fullText.indexOf("Chapter " + (chapterNumber + 1));
+        if (end == -1) {
+            return "";
+        }
+        String chapterText = fullText.substring(fullText.indexOf("Chapter " + chapterNumber), fullText.indexOf("Chapter " + (chapterNumber + 1)));
+        return chapterText;
+    }
+
+    private int getNumOfChapters() throws IOException {
+        String[] arr = fullText.split("\\s+");
+        int count = 0;
+        for (String st : arr) {
+            if (st.equalsIgnoreCase("Chapter")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
     private boolean doesQuoteExist(String currChapterText, String quote) {
         if (currChapterText.contains(quote))
             return true;
         return false;
     }
 
-    public String generateSentence() throws IOException {
-        String currWord = "the";
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 19; i++) {
-            String[] arr = getNextWordList(currWord);
-            String nextWord = getRandomWord(arr);
-            sb.append(nextWord);
-            currWord = nextWord;
-        }
-        return sb.toString();
-    }
 
-    private String getRandomWord(String[] arr) {
-        return null;
+    private String getRandomWord(String[] arr) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        for (String s : arr) {
+            sb.append(s).append(" ");
+        }
+        Map<String, Integer> map = get20MostFrequentWords(sb.toString());
+        String[] keyArr = new String[20];
+        int p = 0;
+        for (String s : map.keySet()) {
+            keyArr[p++] = s;
+        }
+        int max = keyArr.length - 1;
+        int min = 0;
+        int index = (int) ((Math.random() * (max - min)) + min);
+        return keyArr[index];
     }
 
     private String[] getNextWordList(String currWord) throws IOException {
-        int numOfChapters = getNumOfChapters();
-//        int occurrences = getWordFreqInText();
-        for (int i = 0; i < numOfChapters; i++) {
-            String currChapterText = getTextOfChapter(i + 1);
-            getWordList(currChapterText, currWord);
+        String[] arr = fullText.split("[\\p{Punct}\\s]+");
+        int size = getWordFreqInText(fullText, currWord);
+        String[] res = new String[size];
+        int k = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i].equalsIgnoreCase(currWord)) {
+                res[k++] = arr[i + 1];
+            }
         }
-        return null;
+        return res;
     }
 
-    private void getWordList(String currChapterText, String currWord) {
-
-    }
 }
